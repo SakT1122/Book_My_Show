9cf23905c4b17ecffecf1347b85639fcbdb4d46a
SakT1122 <sak.978tirbhane@gmail.com>
2023-03-05 12:38:48 +0530

    adding files

---
+++ src/main/java/com/example/book_my_show/convertors/TheaterConvertors.java
@@ -0,0 +1,13 @@
+package com.example.book_my_show.convertors;
+
+import com.example.book_my_show.Entities.TheaterEntity;
+import com.example.book_my_show.EntryDtos.TheaterEntryDto;
+public class TheaterConvertors {
+
+    public static TheaterEntity convertDtoToEntity(TheaterEntryDto theaterEntryDto){
+
+        return TheaterEntity.builder().location(theaterEntryDto.getLocation())
+                .name(theaterEntryDto.getName()).build();
+
+    }
+}
